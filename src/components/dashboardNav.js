9b29ec40cdd79f3f944ea7c8dc684e9d76126c79
Robert <orazurobert@yahoo.com>
2021-06-10 20:12:40 +0100

    utilized imges

--- src/components/dashboardNav.js
+++ src/components/dashboardNav.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell } from '@fortawesome/free-solid-svg-icons';
+import user from '../images/user.png'
 
 const DashboardNav = () => {
     return (
         <nav className='px-12 flex h-24 justify-between items-center'>
             <p className='primary-font dashboard-side-text text-3xl'>Dashboard</p>
-            <div className=''>
-              <FontAwesomeIcon icon={faBell} />
+            <div className='flex items-center'>
+              <FontAwesomeIcon icon={faBell} className='h-6 border-solid border w-6'/>
+              <img src={user} className='h-8 ml-2 rounded-full' alt='user'/>
             </div>
         </nav>
     )
--- src/components/formSection.js
+++ src/components/formSection.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import Dashes from './dashes'
+import React from 'react';
+import Dashes from './dashes';
+import bank from '../images/bank.png'
+import coin from '../images/coin.png'
 
 const FormSection = ({color, children}) => {
     return (
         <div className='flex h-screen w-full'>
-          <div className={`p-4 text-white ${color} h-full w-3/12`}>
+          <div className={`p-4 text-white ${color} w-3/12`}>
            <Dashes />   
            <p className='text-4xl'>Create multiple sub-account</p>
-           <p className='text-lg'>Organise your business finances easily with multiple accounts. No limits</p>
+           <p className='text-lg mb-14'>Organise your business finances easily with multiple accounts. No limits</p>
+           <img src={coin} alt='coin' className='h-8 float-right mb-8 mr-4 transform -rotate-45'/>
+           <img src={coin} alt='coin' className='h-6 transform mt-24 ml-32 -rotate-90'/>
+           <img src={bank} alt='bank' />
+           <img src={coin} alt='coin' className='h-4 transform -mb-14 ml-24 rotate-12'/>
+           <img src={coin} alt='coin' className='h-6 transform ml-24 -rotate-45'/>
           </div>
-          <div className='w-9/12 flex justify-center mt-14'>
+          <div className='w-9/12 flex justify-center mt-32'>
              {children}
           </div>
-          <p className='absolute bottom-0 left-0 m-8 primary-font text-xl text-white'>© 2020 Prospa Inc</p>
+          {/* <p className='absolute bottom-0 left-0 m-8 primary-font text-xl text-white'>© 2020 Prospa Inc</p> */}
        </div>
     )
 }
