cd4874ce4101b1f84a9edd296435fa683714d7f8
Robert <orazurobert@yahoo.com>
2021-06-11 10:34:03 +0100

    icons for cashflow styling

--- src/components/cashOutflow.js
+++ src/components/cashOutflow.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Spendometer from './spendometer'
+import { bankFees, marketing, internet, transfers } from '../utils/icons'
 
 const CashOutflow = () => {
     return (
         <div className='shadow-md ml-4 p-4 bg-white rounded w-5/12'>
             <p className='primary-font text-base text-purp font-bold'>Cash outflow</p>
-            <Spendometer item='Bank Fees' progress='w-11/12'/>
-            <Spendometer item='Internet' progress='w-8/12'/>
-            <Spendometer item='Marketing' progress='w-4/12'/>
-            <Spendometer item='Transfer' progress='w-3/12'/>
+            <Spendometer item='Bank Fees' progress='w-11/12' icon={bankFees}/>
+            <Spendometer item='Internet' progress='w-8/12' icon={internet}/>
+            <Spendometer item='Marketing' progress='w-4/12' icon={marketing}/>
+            <Spendometer item='Transfer' progress='w-3/12' icon={transfers}/>
         </div>
     )
 }
--- src/components/spendometer.js
+++ src/components/spendometer.js
@@ -1,12 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
 
-const Spendometer = ({item, progress}) => {
+const Spendometer = ({item, progress, icon}) => {
     return (
         <div className='flex items-center justify-between mb-8'> 
-            <p className='dashboard-side-text'>{item}</p>
+            <FontAwesomeIcon icon={icon.component} className={`${icon.style}`}/>
+            <p className='dashboard-side-text text-sm'>{item}</p>
             <div>
-                <p className='text-purp font-semibold'>-N250,000</p>
-                <div className='rounded h-2 w-48 bg-gray-400'>
+                <p className='text-purp font-semibold text-sm'>-N250,000</p>
+                <div className='rounded h-2 w-40 bg-gray-400'>
                     <div className={`rounded h-2 ${progress} bg-yellow-400`}></div>
                 </div>
             </div>   
